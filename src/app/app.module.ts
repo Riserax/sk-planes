943e1bab7cff3688ba7a16e2e22a72d8f6b190e6
kc3 <kamil.cala@softnet.com.pl>
2023-01-11 17:16:10 +0100

    Rozdział 1, lekcje 1-6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAPE52mDmZsuuK0Fco4Jpd_Rkcf_ZaCYw8',
+    authDomain: 'sk-planes-5c7f0.firebaseapp.com',
+    databaseURL: 'https://sk-planes-5c7f0-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'sk-planes-5c7f0',
+    storageBucket: 'sk-planes-5c7f0.appspot.com',
+    messagingSenderId: '572843224346',
+    appId: '1:572843224346:web:c7a20ceae9bafc52cf50a3',
+    measurementId: 'G-FZN561SHKH'
+  }
 };
 
 /*
